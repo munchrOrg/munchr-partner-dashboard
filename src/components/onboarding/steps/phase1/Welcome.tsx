7158e99fba53fcadcb0604a61e6089ad578dde1a
hamzanarinja14myco <hamza.narinja@myco.io>
2026-01-17 18:07:45 +0500

    up

--- src/components/onboarding/steps/phase1/Welcome.tsx
+++ src/components/onboarding/steps/phase1/Welcome.tsx
@@ -1,58 +1,43 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import React from 'react';
+import { useEffect } from 'react';
 import { IntroStep } from '@/components/onboarding/shared/IntroStep';
+import { useGetProfile } from '@/react-query/auth/mutations';
 import { useOnboardingStore } from '@/stores/onboarding-store';
 import { OnboardingPhase } from '@/types/onboarding';
 // import { OnboardingPhase } from '@/types/onboarding';
 
 export function Welcome() {
-  const router = useRouter();
-  const { completedPhases } = useOnboardingStore();
-
   const { profile } = useOnboardingStore();
+  const completedPhases = [];
+  if (profile?.step1) {
+    completedPhases.push(OnboardingPhase.ADD_BUSINESS);
+  }
+  if (profile?.step2) {
+    completedPhases.push(OnboardingPhase.VERIFY_BUSINESS);
+  }
+  if (profile?.step3) {
+    completedPhases.push(OnboardingPhase.OPEN_BUSINESS);
+  }
   console.warn('Profile data in Welcome component:', profile);
-  // const getProfileMutation = useGetProfile();
+  const getProfileMutation = useGetProfile();
 
-  // useEffect(() => {
-  //   getProfileMutation.mutateAsync();
-  // }, []);
-
-  React.useEffect(() => {
-    localStorage.clear();
-    sessionStorage.clear();
-    localStorage.removeItem('onboarding-storage');
-    sessionStorage.removeItem('sentryReplaySession');
-    if (typeof window !== 'undefined') {
-      try {
-        const onboardingStore = useOnboardingStore.getState();
-        onboardingStore.reset();
-      } catch {}
-    }
-    window.onpopstate = () => {
-      router.replace('/sign-up');
-    };
-    return () => {
-      window.onpopstate = null;
-    };
-  }, [router]);
+  useEffect(() => {
+    getProfileMutation.mutateAsync();
+  }, []);
 
   const steps = [
     {
       label: 'Add your business',
-      // completed: !!profile?.step1,
-      completed: completedPhases.includes(OnboardingPhase.ADD_BUSINESS),
+      completed: Boolean(profile?.step1),
     },
     {
       label: 'Verify your business',
-      // completed: !!profile?.step2,
-      completed: completedPhases.includes(OnboardingPhase.VERIFY_BUSINESS),
+      completed: Boolean(profile?.step2),
     },
     {
       label: 'Open your business',
-      // completed: !!profile?.step3,
-      completed: completedPhases.includes(OnboardingPhase.OPEN_BUSINESS),
+      completed: Boolean(profile?.step3),
     },
   ];
 
