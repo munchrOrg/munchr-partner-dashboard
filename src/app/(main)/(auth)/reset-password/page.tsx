a1a50c3924b0ad65a5636bb975ef3a6514c2ba26
hamzanarinja14myco <hamza.narinja@myco.io>
2026-01-20 18:43:57 +0500

    ip

--- src/app/(main)/(auth)/reset-password/page.tsx
+++ src/app/(main)/(auth)/reset-password/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from 'react';
 import { ResetPasswordForm } from '@/components/pages/reset-password/ResetPasswordForm';
 
+export const dynamic = 'force-dynamic';
+
 export default function ResetPasswordPage() {
-  return <ResetPasswordForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordForm />
+    </Suspense>
+  );
 }
