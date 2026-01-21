e220a17a367c9573b440822b42ec5e78b7030a7d
hamzanarinja14myco <hamza.narinja@myco.io>
2026-01-21 11:18:51 +0500

    up

--- src/react-query/portal-users/service.ts
+++ src/react-query/portal-users/service.ts
@@ -3,4 +3,6 @@ import apiClient from '@/lib/axios';
 export const portalUsersService = {
   getAll: () => apiClient.get('/partner/portal-users').then((res) => res.data),
   delete: (id: string) => apiClient.delete(`/partner/portal-users/${id}`).then((res) => res.data),
+  update: (id: string, data: { name: string; roleIds: string[] }) =>
+    apiClient.put(`/partner/portal-users/${id}`, data).then((res) => res.data),
 };
